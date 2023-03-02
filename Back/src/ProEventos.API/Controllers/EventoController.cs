155b97b66757bb942b97f2c5efb1744bc9d4a18a
Mateus Vasconcellos <mateusdepaula3@gmail.com>
2023-03-02 09:23:08 -0300

    Creating first migration with entity framework

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,41 +13,23 @@ namespace ProEventos.API.Controllers
   [Route("api/[controller]")]
   public class EventoController : ControllerBase
   {
-    public IEnumerable<Evento> _evento = new Evento[] {
-        new Evento() {
-        EventoId = 1,
-        Tema = "Angula 11",
-        Local = "Juiz de Fora",
-        Lote = "Primeiro Lote",
-        ImagemUrl = "image.png",
-        QtdPessoas = 250,
-        DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-        },
-        new Evento() {
-        EventoId = 2,
-        Tema = "Angula 12",
-        Local = "Juiz de Fora",
-        Lote = "Primeiro Lote",
-        ImagemUrl = "image.png",
-        QtdPessoas = 350,
-        DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-        }
-      };
-    public EventoController()
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
     {
+      _context = context;
 
     }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-      return _evento;
+      return _context.Eventos;
     }
 
     [HttpGet("{id}")]
-    public IEnumerable<Evento> GetById(int id)
+    public Evento GetById(int id)
     {
-      return _evento.Where(evento => evento.EventoId == id);
+      return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
     }
   }
 }
