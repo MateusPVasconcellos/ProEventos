276261e9baba1b3d072df8c0fc6b575bcb991b31
Mateus Vasconcellos <mateusdepaula3@gmail.com>
2023-03-02 12:53:44 -0300

    Implementing frontend

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -33,6 +33,7 @@ namespace ProEventos.API
           context => context.UseSqlite(Configuration.GetConnectionString("Default"))
       );
       services.AddControllers();
+      services.AddCors();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -54,6 +55,8 @@ namespace ProEventos.API
       app.UseRouting();
 
       app.UseAuthorization();
+
+      app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
       app.UseEndpoints(endpoints =>
       {
